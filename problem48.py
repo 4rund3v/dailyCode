9af91d4521becaa05e976af299cea52bfe96153b
arun dev <4rund3v@gmail.com>
2019-02-24 12:26:17 +0530

    Updated the binary tree problems.

--- problem48.py
+++ problem48.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
 Given pre-order and in-order traversals of a binary tree, write a function to reconstruct the tree.
 
@@ -17,3 +20,60 @@ You should return the following tree:
  / \ / \
 d  e f  g   
 """
+
+
+class Node(object):
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.value = None
+
+
+def print_preorder(root_node):
+    """
+      Given a root node, prints the preorder traversal of that node
+      Pre order traversal:
+          Root --> Left --> Right
+      Use cases :
+          Prefix mathematical expressions
+                       + 2 3
+          Copying a tree
+    """
+    if root_node:
+        print('  {}  '.format(root_node.value))
+        print_preorder(root_node.left)
+        print_preorder(root_node.right)
+    return
+
+
+def print_inorder(root_node):
+    '''
+     Given a root node, prints the inorder traversal of the node
+     Inorder traversal :
+          Left -> Root -> Right
+     Usecases :
+        Binary search trees
+    '''
+    if root_node:
+        print_inorder(root_node.left)
+        print('  {}  '.format(root_node.value))
+        print_inorder(root_node.right)
+    return
+
+
+def print_postorder(root_node):
+    """
+     Given a root node, prints the postorder traversal of it.
+     PostOrder Traversal:
+         Left --> Right --> Root
+     Use cases:
+         Postfix mathematical operations
+                                 2 9 *
+         While deleting a tree, should follow this approach
+    """
+    if root_node:
+        print_postorder(root_node.left)
+        print_postorder(root_node.right)
+        print('  {}  '.format(root_node.value))
+    return
+
--- problem50.py
+++ problem50.py
@@ -15,3 +15,25 @@ For example, given the following tree:
 3  2  4  5
 You should return 45, as it is (3 + 2) * (4 + 5).
 """
+
+class Node(object):
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.value = None
+
+
+
+def print_inorder(root_node):
+    '''
+     Given a root node, prints the inorder traversal of the node
+     Inorder traversal :
+          Left -> Root -> Right
+     Usecases :
+        Binary search trees
+    '''
+    if root_node:
+        print_inorder(root_node.left)
+        print('  {}  '.format(root_node.value))
+        print_inorder(root_node.right)
+    return
