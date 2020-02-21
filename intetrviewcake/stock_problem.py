f5a81fd907957f1ec8e0c31d62b267261721d3e1
Arun Dev <4rund3v@gmail.com>
2020-02-21 11:47:15 +0530

    Interview cake Problem 1 solved

---
+++ intetrviewcake/stock_problem.py
@@ -0,0 +1,135 @@
+"""
+Writing programming interview questions hasn't made me rich yet ... so I might give up and start trading Apple stocks all day instead.
+
+First, I wanna know how much money I could have made yesterday if I'd been trading Apple stocks all day.
+
+So I grabbed Apple's stock prices from yesterday and put them in a list called stock_prices, where:
+
+The indices are the time (in minutes) past trade opening time, which was 9:30am local time.
+The values are the price (in US dollars) of one share of Apple stock at that time.
+So if the stock cost $500 at 10:30am, that means stock_prices[60] = 500.
+
+Write an efficient function that takes stock_prices and returns the best profit I could have made from one purchase and one sale of one share of Apple stock yesterday.
+
+For example:
+
+  stock_prices = [10, 7, 5, 8, 11, 9]
+
+get_max_profit(stock_prices)
+# Returns 6 (buying for $5 and selling for $11)
+
+No "shorting"—you need to buy before you can sell. Also, you can't buy and sell in the same time step—at least 1 minute has to pass.
+"""
+def get_max_profit(stock_prices):
+    if len(stock_prices) < 2:
+        raise ValueError()
+    import sys
+
+    # Calculate the max profit
+    least_stock_price = stock_prices[0]
+    max_stock_price = -(sys.maxsize - 1)
+    for stock_price in stock_prices[1:]:
+        if stock_price < least_stock_price:
+            least_stock_price = stock_price
+            max_stock_price = -(sys.maxsize - 1)
+        elif max_stock_price < stock_price:
+            max_stock_price = stock_price        
+    return  max(max_stock_price-least_stock_price, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_price_goes_up_then_down(self):
+        actual = get_max_profit([1, 5, 3, 2])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_then_up(self):
+        actual = get_max_profit([7, 2, 8, 9])
+        expected = 7
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_up_all_day(self):
+        actual = get_max_profit([1, 6, 7, 9])
+        expected = 8
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_all_day(self):
+        actual = get_max_profit([9, 7, 4, 1])
+        expected = -2
+        self.assertEqual(actual, expected)
+
+    def test_price_stays_the_same_all_day(self):
+        actual = get_max_profit([1, 1, 1, 1])
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_prices(self):
+        with self.assertRaises(Exception):
+            get_max_profit([])
+
+    def test_error_with_one_price(self):
+        with self.assertRaises(Exception):
+            get_max_profit([1])
+
+
+unittest.main(verbosity=2)
+
+
+# actual answer : 
+
+
+  def get_max_profit(stock_prices):
+    if len(stock_prices) < 2:
+        raise ValueError('Getting a profit requires at least 2 prices')
+
+    # We'll greedily update min_price and max_profit, so we initialize
+    # them to the first price and the first possible profit
+    min_price  = stock_prices[0]
+    max_profit = stock_prices[1] - stock_prices[0]
+
+    # Start at the second (index 1) time
+    # We can't sell at the first time, since we must buy first,
+    # and we can't buy and sell at the same time!
+    # If we started at index 0, we'd try to buy *and* sell at time 0.
+    # This would give a profit of 0, which is a problem if our
+    # max_profit is supposed to be *negative*--we'd return 0.
+    for current_time in xrange(1, len(stock_prices)):
+        current_price = stock_prices[current_time]
+
+        # See what our profit would be if we bought at the
+        # min price and sold at the current price
+        potential_profit = current_price - min_price
+
+        # Update max_profit if we can do better
+        max_profit = max(max_profit, potential_profit)
+
+        # Update min_price so it's always
+        # the lowest price we've seen so far
+        min_price  = min(min_price, current_price)
+
+    return max_profit
+
+    
+stock_prices = [10, 7, 5, 8, 11, 9]
+print(get_max_profit(stock_prices))
